1a0b39adc46e72e68aeae18a9c8455a2c0e1f627
Francis Bouvier <francis.bouvier@gmail.com>
2022-10-26 15:26:16 +0200

    Add detail time load in test env
    Signed-off-by: Francis Bouvier <francis.bouvier@gmail.com>

--- src/engine.zig
+++ src/engine.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const v8 = @import("v8");
 
 const utils = @import("utils.zig");
@@ -7,6 +8,10 @@ const gen = @import("generate.zig");
 pub const ExecFunc = (fn (v8.Isolate, v8.ObjectTemplate) anyerror!void);
 
 pub fn Load(comptime execFn: ExecFunc, comptime apis: []gen.API) !void {
+    var start: std.time.Instant = undefined;
+    if (builtin.is_test) {
+        start = try std.time.Instant.now();
+    }
 
     // v8 params
     var params = v8.initCreateParams();
@@ -27,8 +32,44 @@ pub fn Load(comptime execFn: ExecFunc, comptime apis: []gen.API) !void {
     // v8 ObjectTemplate for the global namespace
     const globals = v8.ObjectTemplate.initDefault(isolate);
 
+    var iso_start: std.time.Instant = undefined;
+    if (builtin.is_test) {
+        iso_start = try std.time.Instant.now();
+    }
+
+    // load APIs
     try gen.load(isolate, globals, apis);
-    return execFn(isolate, globals);
+
+    var load_start: std.time.Instant = undefined;
+    if (builtin.is_test) {
+        load_start = try std.time.Instant.now();
+    }
+
+    // exec
+    try execFn(isolate, globals);
+
+    var exec_end: std.time.Instant = undefined;
+    if (builtin.is_test) {
+        exec_end = try std.time.Instant.now();
+    }
+
+    if (builtin.is_test) {
+        const us = std.time.ns_per_us;
+
+        const iso_time = std.time.Instant.since(iso_start, start);
+        const load_time = std.time.Instant.since(load_start, iso_start);
+        const exec_time = std.time.Instant.since(exec_end, load_start);
+        const total_time = std.time.Instant.since(exec_end, start);
+
+        const iso_per = iso_time * 100 / total_time;
+        const load_per = load_time * 100 / total_time;
+        const exec_per = exec_time * 100 / total_time;
+
+        std.debug.print("\nstart of isolate:\t{d}us\t{d}%\n", .{ iso_time / us, iso_per });
+        std.debug.print("load of apis:\t\t{d}us\t{d}%\n", .{ load_time / us, load_per });
+        std.debug.print("exec:\t\t\t{d}us\t{d}%\n", .{ exec_time / us, exec_per });
+        std.debug.print("Total:\t\t\t{d}us\n", .{total_time / us});
+    }
 }
 
 pub const VM = struct {
