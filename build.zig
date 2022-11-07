1013a2a5e6eea11b55f47f080179e0323849a494
Francis Bouvier <francis.bouvier@gmail.com>
2022-11-07 13:17:36 +0100

    Handle linux x86_64 builds
    Signed-off-by: Francis Bouvier <francis.bouvier@gmail.com>

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -8,7 +8,7 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("jsengine", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    linkV8(exe);
+    try linkV8(exe);
     if (mode == .ReleaseSafe) {
         // remove debug info
         // TODO: check if mandatory in release-safe
@@ -30,17 +30,47 @@ pub fn build(b: *std.build.Builder) void {
     const test_exe = b.addTest("src/main.zig");
     test_exe.setTarget(target);
     test_exe.setBuildMode(mode);
-    linkV8(test_exe);
+    try linkV8(test_exe);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_exe.step);
 }
 
-fn linkV8(step: *std.build.LibExeObjStep) void {
+fn linkV8(step: *std.build.LibExeObjStep) !void {
     // step.linkLibC(); // TODO: do we need to link libc?
     // link the static v8 library built with zig-v8
-    // FIXME: we are tied to the native v8 build (aarch64-macos)
-    step.addObjectFile("../zig-v8/v8-build/aarch64-macos/release/ninja/obj/zig/libc_v8.a");
+
+    // v8 library
+    // FIXME: we are tied to native v8 builds, currently:
+    // - aarch64-macos
+    // - x86_64-linux
+    const os = step.target.getOsTag();
+    const arch = step.target.getCpuArch();
+    switch (os) {
+        .linux => blk: {
+            if (arch != .x86_64) {
+                std.debug.print("only x86_64 are supported on linux builds\n", .{});
+                return error.ArchNotSupported;
+            }
+            break :blk;
+        },
+        .macos => blk: {
+            if (arch != .aarch64) {
+                std.debug.print("only aarch64 are supported on macos builds\n", .{});
+                return error.ArchNotSupported;
+            }
+            break :blk;
+        },
+        else => return error.OsNotSupported,
+    }
+    const lib_path = try std.fmt.allocPrint(
+        step.builder.allocator,
+        "../zig-v8/v8-build/{s}-{s}/release/ninja/obj/zig/libc_v8.a",
+        .{ @tagName(arch), @tagName(os) },
+    );
+    step.addObjectFile(lib_path);
+
+    // v8 bindings
     step.addPackagePath("v8", "deps/zig-v8/v8.zig");
     step.addIncludePath("deps/zig-v8");
 }
