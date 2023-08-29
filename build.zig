2e5f40688b97e05af14eb6d5c5bf754e2717b18b
Francis Bouvier <francis.bouvier@gmail.com>
2023-08-29 20:04:34 +0200

    Update build.zig
    Signed-off-by: Francis Bouvier <francis.bouvier@gmail.com>

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.build.Builder) !void {
 
     // TODO: install only bench or shell with zig build <cmd>
 
-    const options = buildOptions(b);
+    const options = try buildOptions(b);
 
     // bench
     // -----
@@ -74,30 +74,54 @@ pub fn build(b: *std.build.Builder) !void {
     test_step.dependOn(&test_exe.step);
 }
 
-pub fn buildOptions(b: *std.build.Builder) *std.build.OptionsStep {
+const Engine = enum {
+    v8,
+};
+
+pub const Options = struct {
+    engine: Engine,
+    opts: *std.build.OptionsStep,
+};
+
+pub fn buildOptions(b: *std.build.Builder) !Options {
     const options = b.addOptions();
     const engine = b.option([]const u8, "engine", "JS engine (v8)");
+    var eng: Engine = undefined;
+    if (engine == null) {
+        // default
+        eng = .v8;
+    } else {
+        if (std.mem.eql(u8, engine.?, "v8")) {
+            eng = .v8;
+        } else {
+            return error.EngineUnknown;
+        }
+    }
     options.addOption(?[]const u8, "engine", engine);
-    return options;
+    return .{ .engine = eng, .opts = options };
 }
 
 fn common(
     step: *std.build.LibExeObjStep,
     mode: std.builtin.Mode,
     target: std.zig.CrossTarget,
-    options: *std.build.OptionsStep,
+    options: Options,
 ) !void {
     step.setTarget(target);
     step.setBuildMode(mode);
-    step.addOptions("jsruntime_build_options", options);
+    step.addOptions("jsruntime_build_options", options.opts);
     step.addPackage(try pkgs.tigerbeetle_io(step));
-    step.addPackage(try pkgs.zig_v8(step));
-    try pkgs.v8(step, mode);
+    if (options.engine == .v8) {
+        try pkgs.v8(step, mode);
+        step.addPackage(try pkgs.zig_v8(step));
+    }
 }
 
 pub fn packages(comptime vendor_path: []const u8) type {
     return struct {
         const Self = @This();
+
+        const vendor = vendor_path ++ "vendor/";
 
         fn tigerbeetle_io(step: *std.build.LibExeObjStep) !std.build.Pkg {
             const lib_path = try std.fmt.allocPrint(
@@ -187,7 +211,7 @@ pub fn packages(comptime vendor_path: []const u8) type {
         pub fn add(
             step: *std.build.LibExeObjStep,
             mode: std.builtin.Mode,
-            options: *std.build.OptionsStep,
+            options: Options,
         ) !void {
             const tigerbeetle_io_pkg = try Self.tigerbeetle_io(step);
             const zig_v8_pkg = try Self.zig_v8(step);
@@ -204,7 +228,7 @@ pub fn packages(comptime vendor_path: []const u8) type {
                 .dependencies = &[_]std.build.Pkg{
                     tigerbeetle_io_pkg,
                     zig_v8_pkg,
-                    options.getPackage("jsruntime_build_options"),
+                    options.opts.getPackage("jsruntime_build_options"),
                 },
             };
             step.addPackage(lib);
